8e1dd179969b48dfea6499d548ea0ee3d4f83631
EvaDM <evamorante1969@gmail.com>
2023-04-17 13:31:15 +0200

    Advice (no code) for logic errors

---
+++ CatchingErrorsLogicAdvice.js
@@ -0,0 +1,9 @@
+/**Logic errors
+
+Unfortunately, there is no way to track these errors other than using your instinct. 
+You can of course use the assumptions logging method (and you should!) but it also means
+that you will need to be able to test the program with values for which you know what the answer will be.
+
+A tip that I can give you and that is based on experience: whenever you work on a 
+program for someone else, don't have the person only give you what the program should
+do. Have the person also give you test values and their results so that you can test that everything works as expected.*/
