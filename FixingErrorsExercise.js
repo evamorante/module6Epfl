c14867a5c7d3bfe364851e91e0c0fda375224e16
EvaDM <evamorante1969@gmail.com>
2023-04-17 13:38:35 +0200

    checking and correcting errors

--- FixingErrorsExercise.js
+++ FixingErrorsExercise.js
@@ -5,7 +5,10 @@ function logIn(passwordIsCorrect) {
     if (passwordIsCorrect != true) {
       console.log("Your password is not correct, you can't log in");
     }
+}
 //it seems that the console.log last statement is an opened door for anyone with wrong passeword could enter in
-    console.log("Welcome back! You are now signed in.");
-  }
+//   console.log("Welcome back! You are now signed in.");
+// }
+
+console.log("Welcome back! You are now signed in.");
   
