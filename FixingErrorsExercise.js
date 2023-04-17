4e3bc36c0fd9b919ff54806184a084c019829e09
EvaDM <evamorante1969@gmail.com>
2023-04-17 13:36:33 +0200

    html file linked to final fixing errors exercise in js

---
+++ FixingErrorsExercise.js
@@ -0,0 +1,11 @@
+// This function takes a boolean parameter
+// that says if password was entered correctly
+// (true = yes, false = no)
+function logIn(passwordIsCorrect) {
+    if (passwordIsCorrect != true) {
+      console.log("Your password is not correct, you can't log in");
+    }
+//it seems that the console.log last statement is an opened door for anyone with wrong passeword could enter in
+    console.log("Welcome back! You are now signed in.");
+  }
+  
